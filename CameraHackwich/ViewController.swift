4313ee3cd19e613a2878cb8768bcb4a36f883aea
joe fleischauer <jfleischauer@rlas-116.org>
2018-12-04 07:12:07 -0600

    Initial Commit

---
+++ CameraHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraHackwich
+//
+//  Created by Fleischauer, Joseph on 12/4/18.
+//  Copyright © 2018 Fleischauer, Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
